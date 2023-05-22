02fe90c178f7c7d412dba2f27a6f4738d1b29add
MatteoMancuso-92 <Matteotrieste@outlook.com>
2023-05-22 14:58:14 +0200

    scss

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,29 @@
 <script>
+import AppNav from './AppNav.vue';
 export default {
- 
-
+  
+  components: {
+    AppNav,
+  }
 }
 </script>
 
 <template>
-  
+  <header>
+    <img src="../assets/img/dc-logo.png" alt="">
+    <AppNav />
+  </header>
 </template>
 
-<style>
+<style lang="scss" scoped>
+@use './../assets/styles/partials/variables' as *;
 
+header{
+  width: 70%;
+  height: 130px;
+  @include align-justify-center;
+  img{
+    height: 90px;
+  } 
+}
 </style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,114 @@
+<script>
+export default {
+    data () {
+    
+    return {
+        navbaritems:[
+            
+            {
+                name: "CHARACTERS",
+                src: "#",
+                active: false,
+            },
+            {
+                name: "COMICS",
+                src: "#",
+                active: false,
+            },
+            {
+                name: "MOVIES",
+                src: "#",
+                active: false,
+            },
+            {
+                name: "TV",
+                src: "#",
+                active: false,
+            },
+            {
+                name: "GAMES",
+                src: "#",
+                active: false,
+            },
+            {
+                name: "COLLECTIBLES",
+                src: "#",
+                active: false,
+            },
+            {
+                name: "VIDEOS",
+                src: "#",
+                active: false,
+            },
+            {
+                name: "FANS",
+                src: "#",
+                active: false,
+            },
+            {
+                name: "NEWS",
+                src: "#",
+                active: false,
+            },
+            {
+                name: "SHOP",
+                src: "#",
+                active: false,
+            },
+        ]
+    }
+}
+}
+</script>
+
+
+
+<template>
+  <nav>
+      <ul>
+        <li v-for="menuItem in navbaritems" :key="menuItem.name">
+				<a :href="menuItem.src">{{ menuItem.name }}</a>
+		</li>
+      </ul>
+    </nav>
+</template>
+
+
+
+<style lang="scss" scoped>
+@use './../assets/styles/partials/variables' as *;
+nav{
+    height: 100%;
+    @include align-center;
+  }
+  
+ul{
+    @include align-center;
+    gap: 1em;
+    list-style: none;
+    height: 100%;
+  }
+
+  
+  li{
+    @include align-center;
+    height: 100%;
+    padding: .5em;
+    font-size: 14px;
+    &:hover{
+      border-bottom: 3px solid $brand-color;
+      cursor: pointer;
+    }
+    &:active{
+      background-color: lightgray;
+    }
+
+    
+    a{
+    text-decoration: none;
+    color: black;
+    }
+  }
+  
+  
+</style>
